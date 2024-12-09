aa1f9d1419e9dba6e741681e913a0fcd84c1050f
2252635 <wy18907660900@gmail.com>
2024-12-09 21:24:35 +0800

    Rank

---
+++ src/components/RankContainer.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="rank-container">
+      <div class="image-background"></div>
+      <div class="space"></div>
+      <div class="content">
+        <RankTop />
+        <Rank />
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import Rank from "./Rank.vue";
+  import RankTop from "./RankTop.vue";
+  </script>
+  
+  <style scoped>
+  .rank-container {
+    position: relative;
+    width: 100%;
+    height: 290px;
+  }
+  
+  .image-background {
+    background-image: url('../assets/book.jpg');
+    background-size: cover;
+    background-position: center;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
+  
+  .content {
+  position: relative; 
+  z-index: 2; 
+  width: 80%;
+  margin-left: auto; 
+  margin-right: auto;
+ }
+
+ .space {
+  height:230px;
+ }
+  </style>
---
+++ src/components/RankTop.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="profile-card">
+      <button class="button borrow">借阅榜</button>
+      <button class="button rating">评分榜</button>
+      <button class="button time">时间榜</button>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.profile-card {
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 1rem;
+  box-shadow: 0 4px 16px rgba(0, 0, 0, 0.25);
+  margin: 0 auto;
+  height: 60px;
+}
+
+.button {
+  padding: 10px 20px;
+  margin-right: 10px; /* 按钮之间的间距 */
+  border: none;
+  border-radius: 0.5rem;
+  color: white;
+  cursor: pointer;
+}
+
+/* 借阅榜按钮 */
+.button.borrow {
+  background-color: #4CAF50; /* 绿色 */
+}
+
+.button.borrow:hover {
+  background-color: #388E3C; /* 深绿色 */
+}
+
+/* 评分榜按钮 */
+.button.rating {
+  background-color: #FF9800; /* 橙色 */
+}
+
+.button.rating:hover {
+  background-color: #F57C00; /* 深橙色 */
+}
+
+/* 时间榜按钮 */
+.button.time {
+  background-color: #2196F3; /* 蓝色 */
+}
+
+.button.time:hover {
+  background-color: #1976D2; /* 深蓝色 */
+}
+</style>
